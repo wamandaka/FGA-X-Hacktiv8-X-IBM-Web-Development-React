76bd8bcf1a1e2e626bc9f4c78108bf185af19f9b
wamandaka <108261088+wamandaka@users.noreply.github.com>
2024-09-09 20:00:22 +0700

    sesi12

---
+++ Sesi12/index.js
@@ -0,0 +1,27 @@
+// var myHeight = 161;
+
+// if (myHeight > 160) {
+//   console.log("bisa masuk");
+// } else {
+//   console.log("tidak bisa masuk");
+// }
+
+// console.log("pulang");
+
+// var ownKTP;
+// var ownSIM;
+
+// if (ownKTP || ownSIM) {
+//   console.log("bisa masuk");
+// }
+
+let myHeight = 161;
+let myAge = 18;
+
+// if (myHeight > 160 && myAge > 17) {
+//   console.log("bisa bikin sim");
+// }
+
+// myAge > 17 && myHeight > 160
+//   ? console.log("bisa bikin sim")
+//   : console.log("tidak bisa bikin sim");
